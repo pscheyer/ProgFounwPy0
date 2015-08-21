38abbd656ca12ed11e6af3f18f252c51e2f27b70
Peter Scheyer <pscheyer@gmail.com>
2015-08-21 10:50:54 -0500

    20150821 1050CST add notes from videos.

--- Lesson 2/rickrolltimerwhileloop.py
+++ Lesson 2/rickrolltimerwhileloop.py
@@ -4,6 +4,7 @@ import webbrowser
 totalbreaks = 3
 breakcount = 0
 
+print("This program started on "+time.ctime())
 while(breakcount < totalbreaks):
     time.sleep(10.0)
     webbrowser.open("http://www.youtube.com/watch?v=dQw4w9WgXcQ")
